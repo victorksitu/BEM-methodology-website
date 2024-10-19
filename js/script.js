90257a9c5688276657142549994d40efc40ced6e
Victor Situ <vsitu011@uottawa.ca>
2024-10-19 18:27:16 -0400

    hero and testimonials section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.classList.toggle('active');
     })
 })
+
+window.addEventListener('scroll', function() {
+    const navbar = this.document.querySelector('.navbar');
+
+    if (this.window.scrollY > 0) {
+        navbar.classList.add('navbar--scroll');
+    } else {
+        navbar.classList.remove('navbar--scroll');
+    }
+})
