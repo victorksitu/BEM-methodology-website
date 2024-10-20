dd84cedd2121360fd63b594a07d730c485264b88
Victor Situ <vsitu011@uottawa.ca>
2024-10-19 21:48:50 -0400

    finished website

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Mobile Menu
     const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
     toggleButton.addEventListener('click', function() {
         mobileMenu.classList.toggle('active');
     })
+
+    // Video Modal
+    const modal = document.getElementById('videoModal');
+    const videoButton = document.querySelector('.preview__video-button');
+    const closeButton = document.querySelector('.modal__close-button');
+    const videoPlayer = document.getElementById('videoPlayer');
+
+    // Open modal on click
+    videoButton.addEventListener('click', function() {
+        modal.style.display = 'block';
+        // replace src for URL
+        videoPlayer.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=Xb96f4roDCuDUbhE';
+    })
+
+    // Close Modal on button click
+    closeButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+        videoPlayer.src = '';
+    })
+
+    // Close Modal on outer click
+    window.addEventListener('click', function() {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    })
+
 })
 
+// Nav on scroll
 window.addEventListener('scroll', function() {
     const navbar = this.document.querySelector('.navbar');
 
